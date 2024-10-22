1fff56b4cc783a44bdb7edbc39f12cc9e797c009
Dylan Jude <dylan.p.jude.ctr@army.mil>
2024-10-22 15:27:56 -0700

    small fix

--- runscript.sh
+++ runscript.sh
@@ -13,13 +13,13 @@ unset UCX_RNDV_SCHEME
 unset UCX_RNDV_FRAG_MEM_TYPES
 unset UCX_RNDV_FRAG_MEM_TYPE
 
-mpiexec -n 2 ./build/bw_and_leak_check H >> log_file.txt
-mpiexec -n 2 ./build/bw_and_leak_check D >> log_file.txt
+mpiexec -n 2 ./bw_and_leak_check H >> log_file.txt
+mpiexec -n 2 ./bw_and_leak_check D >> log_file.txt
 
 export UCX_TLS=cuda,sm
 export UCX_RNDV_SCHEME=put_ppln
 export UCX_RNDV_FRAG_MEM_TYPES=cuda
 export UCX_RNDV_FRAG_MEM_TYPE=cuda
 
-mpiexec -n 2 ./build/bw_and_leak_check H >> log_file.txt
-mpiexec -n 2 ./build/bw_and_leak_check D >> log_file.txt
+mpiexec -n 2 ./bw_and_leak_check H >> log_file.txt
+mpiexec -n 2 ./bw_and_leak_check D >> log_file.txt
